f238ebda80ee7b4e6383e5733ce751c485c998dd
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-07-13 07:12:29 +0530

    update

--- terraform/70-ecr/main.tf
+++ terraform/70-ecr/main.tf
@@ -7,11 +7,3 @@ resource "aws_ecr_repository" "backend" {
   }
 }
 
-resource "aws_ecr_repository" "frontend" {
-  name                 = "${var.project_name}-frontend"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
